858d77c4a4e229b289e33ab5b0875760a347de7d
McCoy Didericksen <mccoy.did@gmail.com>
2023-08-07 20:33:02 -0600

    initial commit

---
+++ src/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Calendar() {
+  return (
+    <div>
+      <iframe className='hero min-h-screen bg-base-200'
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0UorRHpnLx-5IPZ8BOnthdxoGQLi25V_oSsdWwOzmolrTrIL4RCf0EkX6x4Q5OasOo-_0k4H8X?gv=true"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Calendar;
