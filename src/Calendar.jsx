93aa1c8a8cd262615266cdf161297b27ae059988
McCoy Didericksen <mccoy.did@gmail.com>
2023-08-09 10:58:32 -0600

    adding timezone to calendar embed url

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -4,7 +4,7 @@ function Calendar() {
   return (
     <div>
       <iframe className='hero min-h-screen bg-base-200'
-        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0UorRHpnLx-5IPZ8BOnthdxoGQLi25V_oSsdWwOzmolrTrIL4RCf0EkX6x4Q5OasOo-_0k4H8X?gv=true"
+        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0UorRHpnLx-5IPZ8BOnthdxoGQLi25V_oSsdWwOzmolrTrIL4RCf0EkX6x4Q5OasOo-_0k4H8X?gv=true&ctz=America%2FDenver"
       ></iframe>
     </div>
   );
