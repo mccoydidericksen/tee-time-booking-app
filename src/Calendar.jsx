40c32a5758c75d425c37d3ec0edbe9b66e51cae0
McCoy Didericksen <mccoy.did@gmail.com>
2023-08-27 11:29:22 -0600

    updating calendar styles

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -3,26 +3,31 @@ import { InlineWidget } from 'react-calendly';
 
 function Calendar(props) {
   return (
-    <div className="App min-h-screen bg-white">
-      <InlineWidget
-        url={props.person.calendlyLink}
-        pageSettings={{
-          backgroundColor: 'ffffff',
-          hideEventTypeDetails: false,
-          hideLandingPageDetails: true,
-          primaryColor: '00a2ff',
-          textColor: '4d5055',
-        }}
-        prefill={{
-          email: props.person.email,
-          name: props.person.name,
-          customAnswers: {
-            a1: `+1${props.person.phoneNumber}`,
-            a2: '1',
-          },
-        }}
-      />
-    </div>
+    <>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost normal-case text-xl">The Links at Overlake Tee Times</a>
+      </div>
+      <div>
+        <InlineWidget
+          url={props.person.calendlyLink}
+          pageSettings={{
+            backgroundColor: 'ffffff',
+            hideEventTypeDetails: true,
+            hideLandingPageDetails: true,
+            primaryColor: '00a2ff',
+            textColor: '4d5055',
+          }}
+          prefill={{
+            email: props.person.email,
+            name: props.person.name,
+            customAnswers: {
+              a1: `+1${props.person.phoneNumber}`,
+              a2: '1',
+            },
+          }}
+        />
+      </div>
+    </>
   );
 }
 
